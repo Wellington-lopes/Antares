8267caf708834d63e3ea46c564d6af95114ecb58
Wellington Lopes <wellington.slopes1991@gmail.com>
2023-09-06 11:53:17 -0300

    Adicionando JS

---
+++ JS/cliente.js
@@ -0,0 +1,30 @@
+const enderecoForm = document.querySelector(".form-body");
+const cepInput = document.querySelector("#cep");
+const ruaInput = document.querySelector("#rua");
+const bairroInput = document.querySelector("#bairro");
+const cidadeInput = document.querySelector("#cidade");
+const estadoInput = document.querySelector("#estado");
+
+cepInput.addEventListener("keypress", (e) =>{
+    const onlyNumbers = /[0-9]/;
+    const tecla = String.fromCharCode(e.keyCode);
+    if (!onlyNumbers.test(tecla)){
+        e.preventDefault();
+        return;
+    }
+});
+
+cepInput.addEventListener("keyup", (e) =>{
+    const inputValue = e.target.value;
+    if (inputValue.length === 8){
+        //fazer alguma coisa - rodar a API
+    }
+});
+
+const getEndereco = async (cep) =>{
+
+    const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+}
