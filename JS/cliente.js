13d9097e0f1a032e9b42d4d38876154495ccdda1
Wellington Lopes <wellington.slopes1991@gmail.com>
2023-09-11 11:49:11 -0300

    Add files via upload

--- JS/cliente.js
+++ JS/cliente.js
@@ -1,30 +1,39 @@
-const enderecoForm = document.querySelector(".form-body");
-const cepInput = document.querySelector("#cep");
-const ruaInput = document.querySelector("#rua");
-const bairroInput = document.querySelector("#bairro");
-const cidadeInput = document.querySelector("#cidade");
-const estadoInput = document.querySelector("#estado");
+var enderecoForm = document.querySelector(".form-body");
+var cepInput = document.querySelector("#cep");
+var ruaInput = document.querySelector("#rua");
+var bairroInput = document.querySelector("#bairro");
+var cidadeInput = document.querySelector("#cidade");
+var estadoInput = document.querySelector("#estado");
+var formInput = document.querySelector("[data-input]"); // é um array, quando vem do site vem como uma lista/ vamos ter que desmembrar
 
-cepInput.addEventListener("keypress", (e) =>{
+/*cepInput.addEventListener("keypress", (e) =>{
     const onlyNumbers = /[0-9]/;
     const tecla = String.fromCharCode(e.keyCode);
     if (!onlyNumbers.test(tecla)){
         e.preventDefault();
         return;
     }
-});
+});*/
 
 cepInput.addEventListener("keyup", (e) =>{
     const inputValue = e.target.value;
     if (inputValue.length === 8){
-       getEndereco(inputValue);
+       getEndereco(inputValue); //chama uma função
     }
 });
 
-const getEndereco = async (cep) =>{
-    cepInput.blur();
+
+var getEndereco = async (cep) =>{
+    //cepInput.blur();
     const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
+    console.log(formInput);
+    console.log(data.erro);
+    ruaInput.value = data.logradouro;
+    cidadeInput.value = data.localidade;
+    bairroInput.value = data.bairro;
+    estadoInput.value = data.uf;
+
 }
